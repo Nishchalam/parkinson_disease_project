314bf18aa504a093d66fc34fe899f4a4ac177585
Nishchala Mukku <mnishchala@gmail.com>
2026-02-22 01:25:12 +0530

    Web app is ready to use with proper UI!

--- app.py
+++ app.py
@@ -22,6 +22,14 @@ def predict_api():
     print(output[0])
     return jsonify(int(output[0]))
 
+@app.route('/predict', methods=["POST"])
+def predict():
+    data=[float (x) for x in request.form.values() ]
+    final_input=scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=model.predict(final_input)[0]
+    return render_template('home.html', prediction_text="The status of the person is {}".format(output))
+
 @app.route('/about')
 def about():
     about_info = {
